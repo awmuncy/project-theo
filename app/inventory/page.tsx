b8c0a941aa9eeab4a58ee4158b8a87024698a597
Allen Muncy <allen@ressiosoftware.com>
2024-12-03 16:39:29 -0600

    Add inventory page

---
+++ app/inventory/page.tsx
@@ -0,0 +1,11 @@
+import { InventoryItem } from "@/components/InventoryItem";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main className="main">
+        <InventoryItem />
+      </main>
+    </div>
+  );
+}
---
+++ components/InventoryItem.tsx
@@ -0,0 +1,7 @@
+export function InventoryItem() {
+  return (
+    <div className="inventory-item">
+      <h1>Inventory Item</h1>
+    </div>
+  );
+}
