692f14ba708bd2ce4191ebd27da337647eb4192d
Allen Muncy <allen@ressiosoftware.com>
2025-01-08 17:48:46 -0600

    Allow for testing on pages with clerk

--- jest.config.ts
+++ jest.config.ts
@@ -74,7 +74,11 @@ const config: Config = {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  globals: {
+    fetch: global.fetch,
+    Request: global.Request,
+    Response: global.Response,
+  },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -97,7 +101,10 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "#crypto":
+      "<rootDir>/node_modules/@clerk/backend/dist/runtime/node/crypto.js",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
