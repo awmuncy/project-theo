247d96f3c0ab6f4cfba39055daeabd9526902a6e
Allen Muncy <allen@ressiosoftware.com>
2025-01-21 17:33:32 -0600

    Landing page plans

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -4,8 +4,14 @@ export default function LandingPage() {
   return (
     <div className={lp.landingPage}>
       <h1>Welcome to Project Theo</h1>
-      <p>A web based game from trading assets</p>
-      <div>IDK man you are not signed in okay.</div>
+      <p>
+        Here we will put a big banner with a cool background and a button to
+        sign in.
+      </p>
+      <p>
+        Here we will but a section with no BG with a paragraph about the game
+      </p>
+      <p>Finally, we will but a PNG of a single creature above the footer.</p>
     </div>
   );
 }
